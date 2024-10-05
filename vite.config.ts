ab979d83dcbe7d9f32363e74640da232fa787ee6
richard <rwlane999@gmail.com>
2024-10-05 08:07:22 -0400

    main. initial commit. package published to npmjs.

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+//
+import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.tsx'),
+      name: 'MyReactPackage',
+      fileName: (format) => `MyReactPackage.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
